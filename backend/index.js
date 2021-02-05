4902845353d8148ce935938d621656f361fa051b
pacis <pacis@gmail.com>
2021-02-05 16:20:01 +0200

    todo-app

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Origin', "https://todo-app-pacis.web.app/");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers',  'Content-Type');
    next();
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(require("./routes/createTodo"))
 
-mongoose.connect("mongodb://localhost/mern-crud",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:true})
+mongoose.connect("mongodb+srv://pacis:stephencurry@nodetutorial.fqo9t.mongodb.net/mern-crud?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:true})
   .then(()=>{console.log("Connected to mongodb successfully...")})
   .catch(err=>{console.log("Failed to connect to mongodb due to ",err)})
 
@@ -24,4 +24,4 @@ const port = process.env.PORT || 4000
 
 app.listen(port,()=>{
    console.log(`listening to port ${port}....`)
-})
+})                                                                    
